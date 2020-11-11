3f3a744ee9fc63ef4cff913186f9e59c59f573dc
Ekaterina Matveyeva <grinteabox@gmail.com>
2020-11-11 14:26:39 +0200

    add: add ready project to github

---
+++ js/PopupSignUp.js
@@ -0,0 +1,28 @@
+/**
+ * Created by l e n o v o on 29.01.2018.
+ */
+$(document).ready(function(){
+    PopUpHide();
+});
+function PopUpShow(){
+    $("#popup1").show();
+}
+function PopUpHide(){
+    $("#popup1").hide();
+}
+
+//<script src="http://code.jquery.com/jquery-2.0.2.min.js"></script>
+//<script>
+//$(document).ready(function(){
+//    //Скрыть PopUp при загрузке страницы
+//    PopUpHide();
+//});
+////Функция отображения PopUp
+//function PopUpShow(){
+//    $("#popup1").show();
+//}
+////Функция скрытия PopUp
+//function PopUpHide(){
+//    $("#popup1").hide();
+//}
+//</script>
